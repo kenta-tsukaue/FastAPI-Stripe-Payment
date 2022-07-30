16aa7caa8b1961d8230fd774ff0563705fe3b68c
kenta-tsukaue <tsukaue.k@arklet.jp>
2022-07-31 08:52:32 +0900

    databaseの初期設定変更

--- databases.py
+++ databases.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import (sessionmaker, relationship, scoped_session)
 sys.dont_write_bytecode = True
 
 #setting db connection
-url = "mysql+pymysql://root:password@localhost:3306/fastapi?charset=utf8"
+url = "mysql+pymysql://root:[your_password]@localhost:3306/[database_name]?charset=utf8"
 engine = create_engine(url, echo=False, pool_recycle=10)
 
 #create session
